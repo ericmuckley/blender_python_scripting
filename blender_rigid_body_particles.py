e462a15fb7580e1f7579b1c4bae9bef7a3c6ec24
Eric Muckley <ericmuckley@gmail.com>
2019-12-30 14:28:23 -0500

    Add files via upload

--- blender_rigid_body_particles.py
+++ blender_rigid_body_particles.py
@@ -20,10 +20,14 @@ Convenience Variables: C = bpy.context, D = bpy.data
 To debug a python script, run blender script from the command line:
 blender --background --python blender_molecules00.py
 
+For obtaining location data from rigid body object at each keyframe:
+obj = bpy.data.objects["Cube"]
+print(obj.matrix_world.translation)
+
+
+
 """
 
-# enable rigid body world
-#bpy.ops.rigidbody.world_add()
 
 
 def delete_all_objects_and_materials():
@@ -32,10 +36,21 @@ def delete_all_objects_and_materials():
     # select all objects and delete them
     bpy.ops.object.select_all(action='SELECT')
     bpy.ops.object.delete(use_global=False, confirm=False)
+    # delete all physics bakes
+    bpy.ops.ptcache.free_bake_all()
     # delete all materials
     for material in D.materials:
         material.user_clear()
         D.materials.remove(material)
+
+
+
+def add_camera(loc=(0, 0, 20), rot=(0, 0, 0)):
+    """Add a camera to the scene."""
+    bpy.ops.object.camera_add(location=loc, rotation=rot)
+    C.object.name = 'cam'
+    C.scene.camera = D.objects['cam']
+
 
 
 def boundary_plane(size, loc=(0, 0, 0), rot=(0, 0, 0),
@@ -56,7 +71,7 @@ def boundary_plane(size, loc=(0, 0, 0), rot=(0, 0, 0),
     # make collisions elastic
     C.object.rigid_body.restitution = 1
     C.object.rigid_body.friction = 0
-    #C.object.rigid_body.collision_margin = 0.005
+    C.object.rigid_body.collision_margin = 0.01
     #C.object.rigid_body.mass = 100
     bpy.ops.object.modifier_add(type='SOLIDIFY')
     C.object.modifiers["Solidify"].thickness = 0.1
@@ -129,8 +144,10 @@ def create_rigidbody_particle(loc=(0, 0, 0), radius=1, name=None,
     bpy.ops.rigidbody.objects_add()
     C.object.rigid_body.restitution = 1
     C.object.rigid_body.friction = 0
+    C.object.rigid_body.linear_damping = 0
+    C.object.rigid_body.angular_damping = 0
     C.object.display.show_shadows = False
-    #C.object.rigid_body.collision_margin = 100
+    C.object.rigid_body.collision_margin = 0.01
     C.object.rigid_body.collision_shape = 'SPHERE'
     # change particle mass
     if mass:
@@ -150,16 +167,11 @@ def create_rigidbody_particle(loc=(0, 0, 0), radius=1, name=None,
 
 delete_all_objects_and_materials()
 
+add_camera(loc=(14, -14, 6), rot=(np.pi/2.5, 0, np.pi/4))
+
 # add light source
 bpy.ops.object.light_add(type='SUN', radius=1.0, location=(0, 2, 5))
 C.object.name = 'lamp'
-
-# add camera
-cam_pos = [0, 0, 20]
-cam_rot = [0, 0, 0]
-bpy.ops.object.camera_add(location=cam_pos, rotation=cam_rot)
-C.object.name = 'cam'
-
 
 # make background black
 bg = D.worlds["World"].node_tree.nodes["Background"]
@@ -168,30 +180,6 @@ bg.inputs[0].default_value = (0, 0, 0, 0)
 # set gravitational acceleration vector
 C.scene.gravity = [0, 0, 0]
 
-C.scene.rigidbody_world.steps_per_second = 100
-C.scene.rigidbody_world.solver_iterations = 20
-
-#C.space_data.context = 'RENDER'
-#C.scene.render.engine = 'CYCLES'
-
-
-
-# enable rigid bodies in the world
-#bpy.ops.rigidbody.world_add()
-
-#bpy.context.space_data.context = 'OUTPUT'
-#bpy.context.scene.render.filepath = "/home/eric/Desktop/blender_render"
-
-'''
-C.space_data.context = 'OUTPUT'
-C.space_data.system_bookmarks_active = 1
-
-C.space_data.params.filename = "blender_render"
-C.scene.render.image_settings.file_format = 'AVI_JPEG'
-C.scene.render.image_settings.quality = 100
-'''
-
-
 
 # ------------------------ INITIALIZE KEYFRAMES ------------------------------
 
@@ -199,14 +187,10 @@ C.scene.render.image_settings.quality = 100
 start_kf, end_kf = 0, 250
 C.scene.frame_start = start_kf
 C.scene.frame_end = end_kf
-
-
-
 # for animation, track current frame, specify desired number of key frames
 current_kf = C.scene.frame_start
 # set the scene to the current frame
 C.scene.frame_set(current_kf)
-
 
 # --------------------------- CREATE PARTICLES -------------------------------
 
@@ -218,14 +202,11 @@ mat = make_gas_material()
 for i in range(gas_particle_num):
     create_rigidbody_particle(
         loc=(np.random.random(3)-0.5)/10,
-        radius=0.002,
+        radius=0.001,
         mass=20,
         name='gas_particle_' + str(i).zfill(3),
         mat=mat)
-particles = [p for p in D.objects if p.name.startswith('gas_particle')]
-    
-
-
+particles = [p for p in D.objects if p.name.startswith('particle')]
 
 
 # initialize keyframes for each particle
@@ -276,3 +257,18 @@ for i in range(0, 250):
     # increment the keyframe
     current_kf += 1
 .'''
+
+
+
+# ------------------------------------------------------
+
+C.scene.render.filepath = "/home/eric/Desktop/blender_render"
+C.scene.render.image_settings.file_format = 'AVI_JPEG'
+C.scene.render.image_settings.quality = 100
+C.scene.rigidbody_world.steps_per_second = 300
+C.scene.rigidbody_world.solver_iterations = 50
+
+RENDER = True
+if RENDER:
+    bpy.ops.ptcache.bake_all(bake=True)
+    bpy.ops.render.render('INVOKE_DEFAULT', animation=True)
